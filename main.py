e5438e2879d5c17e68f677c18a2ec43f13df304d
Mike Kiruti <mikekirutic@gmail.com>
2022-12-07 20:11:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+
+# create screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+# create paddles
+scores = Scoreboard()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+# create ball
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    #detect collisions with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    # detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+
+    # award players
+    # right paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scores.l_point()
+        print("L player score")
+    # left paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scores.r_point()
+        print("R player score")
+
+screen.exitonclick()
